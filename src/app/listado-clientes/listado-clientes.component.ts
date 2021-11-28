1f281cbf973d8674bc7721bcec98017250fc3b22
katamus <christian.catamuscay.h@gmail.com>
2021-11-28 18:23:17 -0500

    Ejemplo de obtencion de llave del item y la referencia de  firebase

--- src/app/listado-clientes/listado-clientes.component.ts
+++ src/app/listado-clientes/listado-clientes.component.ts
@@ -11,9 +11,37 @@ export class ListadoClientesComponent implements OnInit {
   constructor(private db: AngularFirestore) { }
 
   ngOnInit(): void {
+    /*
     this.db.collection('Clientes').valueChanges().subscribe((resultado)=>{
       this.clientes = resultado;
     });
+    */
+
+    this.clientes.length = 0;
+    this.db.collection('Clientes').get().subscribe((resultado)=>{
+      console.log(resultado.docs);
+
+        resultado.docs.forEach((item)=>{
+          let cliente:any = item.data();
+          cliente.id = item.id;
+          cliente.ref = item.ref;
+          this.clientes.push(cliente);
+        })
+        /*
+        for (let item of resultado.docs) {
+          console.log(item.id);
+          console.log(item.data());
+          console.log(item.ref);
+        }
+        */
+
+
+    })
+
+
+
+
+    
   }
 
 }
