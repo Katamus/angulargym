6d58f388a40d0add649914089d2121939b48a424
katamus <christian.catamuscay.h@gmail.com>
2021-12-08 09:51:42 -0500

    Ejemplo de editar  un registro de firebase  con su objeto modelo del valor a modificar

--- src/app/precios/precios.component.ts
+++ src/app/precios/precios.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Precio } from '../models/precios';
 import { MensakesService } from '../service/mensakes.service';
 
 @Component({
@@ -10,7 +11,9 @@ import { MensakesService } from '../service/mensakes.service';
 })
 export class PreciosComponent implements OnInit {
   formularioPrecio!:FormGroup;
-  precios:any[] = new Array<any>();
+  esEditar:boolean = false;
+  id!:string;
+  precios:Precio[] = new Array<Precio>();
   constructor( private fb: FormBuilder, private db: AngularFirestore, private msg: MensakesService) { }
 
   ngOnInit(): void {
@@ -20,22 +23,50 @@ export class PreciosComponent implements OnInit {
       duracion:['', Validators.required],
       tipoDuracion:['', Validators.required]
     });
+    this.mostrarPrecios();
+  }
+
+  agregar(){
+    this.db.collection<Precio>('precios').add(this.formularioPrecio.value).then(()=>{
+      this.msg.mensajeCorrecto('Agregado','Se agrego correctamente');
+      this.formularioPrecio.reset();
+      this.mostrarPrecios();
+    }).catch(()=>{
+      this.msg.mensajeError('Error','Ocurrio un error');
+    });
+  }
+
+  editarPrecio(precio:Precio){
+    this.esEditar = true;
+    this.formularioPrecio.setValue({
+      nombre:precio.nombre,
+      costo:precio.costo,
+      duracion:precio.duracion,
+      tipoDuracion:precio.tipoDuracion
+    });
+    this.id = precio.id;
+  }
+
+  editar(){
+    this.db.doc('precios/'+this.id).update(this.formularioPrecio.value).then(()=>{
+      this.msg.mensajeCorrecto('Editado','Se edito correctamente');
+      this.formularioPrecio.reset();
+      this.esEditar = false;
+      this.mostrarPrecios();
+    }).catch(()=>{
+      this.msg.mensajeCorrecto('Error','Error al editar');
+    });
+  }
+
+  mostrarPrecios(){
+    this.precios.length= 0;
     this.db.collection('precios').get().subscribe((resultado)=>{
       resultado.forEach((dato)=>{
-        let precio:any  = dato.data();
+        let precio:any  = dato.data() as Precio;
         precio.id = dato.id;
         precio.ref = dato.ref;
         this.precios.push(precio);
       })
-    });
-  }
-
-  agregar(){
-    this.db.collection('precios').add(this.formularioPrecio.value).then(()=>{
-      this.msg.mensajeCorrecto('Agregado','Se agrego correctamente');
-      this.formularioPrecio.reset();
-    }).catch(()=>{
-      this.msg.mensajeError('Error','Ocurrio un error');
     });
   }
 
