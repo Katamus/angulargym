77d80ba449a2bcef97bbaa32d762b89dbe385f17
katamus <christian.catamuscay.h@gmail.com>
2021-12-15 20:14:05 -0500

    Guardando la subcripcion a firebase el cual toco enviarle un objeto y no una clase por que las referencia a otras colecciones no dejeron eso creo

--- src/app/inscripcion/inscripcion.component.ts
+++ src/app/inscripcion/inscripcion.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { Cliente } from '../models/cliente';
 import { Inscripcion } from '../models/inscripcion';
+import { Precio } from '../models/precios';
+import { MensakesService } from '../service/mensakes.service';
 
 @Component({
   selector: 'app-inscripcion',
@@ -9,9 +12,21 @@ import { Inscripcion } from '../models/inscripcion';
 })
 export class InscripcionComponent implements OnInit {
   clienteSeleccionado:Cliente = new Cliente();
-  constructor() { }
+  precioSeleccionado:Precio = new Precio();
   inscripcion: Inscripcion = new Inscripcion();
+  precios: Precio[] = new Array<Precio>();
+  idPrecio:string = "null";
+  constructor( private db:AngularFirestore, private msj:MensakesService ) { }
+  
   ngOnInit(): void {
+    this.db.collection('precios').get().subscribe((resultado)=>{
+      resultado.docs.forEach((item)=>{
+        let precio:any = item.data() as Precio;
+        precio.id = item.id;
+        precio.ref = item.ref;
+        this.precios.push(precio);
+      })
+    })
   }
 
   asignarCliente(cliente:Cliente){
@@ -21,11 +36,97 @@ export class InscripcionComponent implements OnInit {
 
   eliminarCliente(){
     this.clienteSeleccionado =  new Cliente();
-    this.inscripcion = new Inscripcion();
+    this.inscripcion.cliente = null;
   }
 
   guardar(){
-    console.log(this.inscripcion);
+    if( this.inscripcion.validar().esValido ){
+
+      let inscripcionAgregar = {
+        fecha : this.inscripcion.fecha,
+        fechaFinaliza : this.inscripcion.fechaFinaliza,
+        cliente : this.inscripcion.cliente,
+        precios : this.inscripcion.precios,
+        subTotal : this.inscripcion.subTotal,
+        isv : this.inscripcion.isv,
+        total : this.inscripcion.total
+      }
+
+
+      this.db.collection('inscripciones').add(inscripcionAgregar).then((resultado)=>{
+        this.inscripcion = new Inscripcion();
+        this.clienteSeleccionado = new Cliente();
+        this.precioSeleccionado = new Precio();
+        this.idPrecio = "null";
+        this.msj.mensajeCorrecto("Inscripción","se guardo Correctamente");
+      }).catch(()=>{
+        this.msj.mensajeCorrecto("Inscripción","Error en el Guardado");
+      });
+    }else{
+      this.msj.mensajeAdvertencia("Inscripción",this.inscripcion.validar().mensaje);
+    }
+  }
+
+  seleccionarPrecio(target:any){
+    if( target.value != "null" ){
+      this.precioSeleccionado = this.precios.find( item => item.id == target.value )!;
+      console.log(this.precioSeleccionado);
+      this.inscripcion.precios = this.precioSeleccionado.ref
+      this.inscripcion.fecha = new Date();
+      let dias!:number;
+      let meses!:number;
+      let anios!:number;
+      console.log(this.precioSeleccionado.tipoDuracion);
+      let tipoDuracion:number = this.precioSeleccionado.tipoDuracion;
+      switch (~~tipoDuracion) {
+        case 1:
+          dias = this.precioSeleccionado.duracion;
+          this.inscripcion.fechaFinaliza = this.sumarDiasFecha(this.inscripcion.fecha,dias);
+          break;
+        case 2:
+          dias = this.precioSeleccionado.duracion * 7;
+          this.inscripcion.fechaFinaliza = this.sumarDiasFecha(this.inscripcion.fecha,dias);
+          break;
+        case 3:
+          dias = this.precioSeleccionado.duracion * 15;
+          this.inscripcion.fechaFinaliza = this.sumarDiasFecha(this.inscripcion.fecha,dias);
+          break;
+        case 4:
+          meses = this.precioSeleccionado.duracion ;
+          this.inscripcion.fechaFinaliza = this.sumarMesesFecha(this.inscripcion.fecha,meses);
+          break;
+        case 5:
+          anios = this.precioSeleccionado.duracion ;
+          this.inscripcion.fechaFinaliza = this.sumarAnioFecha(this.inscripcion.fecha,anios);
+            break;
+        default:
+            break;
+      }
+
+      this.inscripcion.subTotal = this.precioSeleccionado.costo;
+      this.inscripcion.isv = this.inscripcion.subTotal * 0.15;
+      this.inscripcion.total = this.inscripcion.subTotal + this.inscripcion.isv;
+    }else{
+      this.precioSeleccionado = new Precio();
+      this.inscripcion.fecha = null;
+      this.inscripcion.fechaFinaliza = null;
+      this.inscripcion.precios = null;
+      this.inscripcion.subTotal = 0;
+      this.inscripcion.isv = 0;
+      this.inscripcion.total = 0;
+    }
+  }
+
+  sumarDiasFecha( fechaInicial:Date, cantidadDias:number){
+      return new Date(fechaInicial.getFullYear(), fechaInicial.getMonth(),fechaInicial.getDate() + cantidadDias );
+  }
+
+  sumarMesesFecha( fechaInicial:Date, cantidadMeses:number){
+    return new Date(fechaInicial.getFullYear(), fechaInicial.getMonth() + cantidadMeses,fechaInicial.getDate());
+  }
+
+  sumarAnioFecha( fechaInicial:Date, cantidadAnio:number){
+    return new Date(fechaInicial.getFullYear() + cantidadAnio, fechaInicial.getMonth(),fechaInicial.getDate());
   }
 
 }
--- src/app/models/inscripcion.ts
+++ src/app/models/inscripcion.ts
@@ -2,7 +2,7 @@ import { DocumentReference } from "@angular/fire/firestore";
 
 export class Inscripcion{
     fecha!: Date;
-    fechaFinal!: Date;
+    fechaFinaliza!: Date;
     cliente!: DocumentReference;
     precios!: DocumentReference;
     subTotal!: number;
@@ -10,7 +10,44 @@ export class Inscripcion{
     total!:number;
 
     constructor(){
-        
+        this.fecha = null;
+        this.fechaFinaliza = null;
+        this.cliente = this.cliente!;
+        this.precios = this.precios!;
+        this.subTotal = this.subTotal!;
+        this.isv = this.isv!;
+        this.total = this.total!;
+    }
+
+    validar(): any{
+        let respuesta = {
+            esValido : true,
+            mensaje:''
+        }
+        if( this.cliente == null || this.cliente == undefined){
+            respuesta.esValido = false;
+            respuesta.mensaje = 'Seleccione un cliente'
+        }else if( this.precios == null || this.precios == undefined){
+            respuesta.esValido = false;
+            respuesta.mensaje = 'No ha seleccionado un precio'
+        }
+        else if( this.fecha == null || this.fecha == undefined){
+            respuesta.esValido = false;
+            respuesta.mensaje = 'No tiene fecha inicio'
+        }else if( this.fechaFinaliza == null || this.fechaFinaliza == undefined){
+            respuesta.esValido = false;
+            respuesta.mensaje = 'No tiene fecha final'
+        } else if( this.subTotal == null || this.subTotal <= 0 || this.subTotal == undefined){
+            respuesta.esValido = false;
+            respuesta.mensaje = 'No se ha podido calcular el sub total'
+        } else if( this.isv == null || this.isv <= 0 || this.isv == undefined){
+            respuesta.esValido = false;
+            respuesta.mensaje = 'No se ha podido calcular el isv'
+        } else if( this.total == null || this.total <= 0 || this.total == undefined){
+            respuesta.esValido = false;
+            respuesta.mensaje = 'No se ha podido calcular el total'
+        } 
+        return respuesta;
     }
 
 
